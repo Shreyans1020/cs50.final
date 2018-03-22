e5be2801818836539583bc0e2dc19aee013baa7b
Shreyans1020 <31969745+Shreyans1020@users.noreply.github.com>
2018-03-22 14:49:10 -0400

    Create investor.py

---
+++ investor.py
@@ -0,0 +1,21 @@
+# investors portfolio - trying to make a portfolio to help an investor to invest in stocks
+# Names - Nick Neuber, Shreyans Saraogi
+
+
+class Investor():
+    firstName = ''
+    lastName = ''
+    email = ''
+    Bday = ''
+    money = ''
+
+    def __init__(self, firstName, lastName, email, Bday, money):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.Bday = Bday
+        self.money = money
+
+
+
+
