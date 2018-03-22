8481ab482c168e2b4894306c43fd49daf61a4b7a
Shreyans1020 <31969745+Shreyans1020@users.noreply.github.com>
2018-03-22 14:49:31 -0400

    Create stocks.py

---
+++ stocks.py
@@ -0,0 +1,13 @@
+# Purpose - To make a portfolio pf stocks and link a database
+# Programmers - Nick Neuber, shryeans Saraogi
+
+import openpyxl
+
+
+class Stocks():
+
+    def __init__(self):
+        pass
+
+    def database(self):
+        print("hi1")
