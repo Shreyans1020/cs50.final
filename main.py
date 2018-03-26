8ac26ddb25261ff82d476cee92c749f58e50ce48
Shreyans1020 <31969745+Shreyans1020@users.noreply.github.com>
2018-03-26 14:23:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 #Shreyans Saraogi and Nick Neuber
 
-import cs50
+
 import openpyxl
-
-from investor import Investor
-from stocks import Stocks
-
 
 def main():
 
@@ -15,27 +11,24 @@ def main():
     lastName = input(str("Enter your last name: "))
     email = input(str("Enter your email address: "))
     Bday = input(str("Enter your date of birth: "))
-    print("Enter the amount of money you wish to simulate:", end = "")
-    money = cs50.get_int()
-
+    money = input(str("Enter the amount of money you want to invest - "))
 
 
     print("--------------------------------------------")
 
-    while True:
-        print("Do you want to 1) Research a Stock or 2) Make a Portfolio")
-        print("1 or 2?   ", end = "")
-        choice = cs50.get_int()
-        if ((choice == 1) or (choice == 2)):
-            break
+    #while True:
+     #   print("Do you want to 1) Research a Stock or 2) Make a Portfolio")
+      #  print("1 or 2?   ", end = "")
+       # choice = cs50.get_int()
+        #if ((choice == 1) or (choice == 2)):
+            #break
     stock = input(str("Which company's stock are you interested in?   "))
     excel_document = openpyxl.load_workbook('Stock_Information.xlsx')
         #Database linked
-
     sheet = excel_document.get_sheet_by_name('Sheet1')
-    print (type(excel_document))
-    Investor(firstName, lastName, email, Bday, money)
-    Stocks()
+    print (sheet['A2'].value)
+    #Investor(firstName, lastName, email, Bday, money)
+    #Stocks()
 
 
 
@@ -43,5 +36,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
-
